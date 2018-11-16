61e8d5ed460ffe38577438ee9ebeb16c7cf219aa
Reggie Bigornia <reggietheroman@gmail.com>
2018-11-17 03:14:06 +0800

    Configured project to use pods.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'marreg',
+    podModulePrefix: 'marreg/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
